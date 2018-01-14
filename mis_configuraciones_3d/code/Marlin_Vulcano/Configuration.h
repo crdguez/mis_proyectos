7c819c68355c35b43322f1251e1c884847d09e73
crdguez <carlos.rodriguez.jaso@gmail.com>
2018-01-14 20:24:39 +0100

    marlin

--- mis_configuraciones_3d/code/Marlin_Vulcano/Configuration.h
+++ mis_configuraciones_3d/code/Marlin_Vulcano/Configuration.h
@@ -410,7 +410,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
   #define X_PROBE_OFFSET_FROM_EXTRUDER -0
   #define Y_PROBE_OFFSET_FROM_EXTRUDER -0
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.8
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.3
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
